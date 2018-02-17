792e40e97a4bb95e31d648de55c060d941ff07b9
Eugene Burmako <eburmako@twitter.com>
2018-02-17 11:41:44 -0800

    Upgrade to Scalameta 3.3.0

--- sbthost/nsc/src/main/scala/scala/meta/internal/sbthost/SymbolInformationOps.scala
+++ sbthost/nsc/src/main/scala/scala/meta/internal/sbthost/SymbolInformationOps.scala
@@ -4,6 +4,7 @@ import scala.reflect.internal.{Flags => gf}
 import scala.meta.internal.{semanticdb3 => s}
 import scala.meta.internal.semanticdb3.SymbolInformation.{Kind => k}
 import scala.meta.internal.semanticdb3.SymbolInformation.{Property => p}
+import scala.meta.internal.semanticdb3.Accessibility.{Tag => a}
 
 trait SymbolInformationOps { self: DatabaseOps =>
   import g._
@@ -12,7 +13,7 @@ trait SymbolInformationOps { self: DatabaseOps =>
     private val gsym: g.Symbol = {
       if (gsym0.isModuleClass) gsym0.asClass.module
       else if (gsym0.isTypeSkolem) gsym0.deSkolemize
-      else gsym0.setter(gsym0.owner).orElse(gsym0.getter(gsym0.owner).orElse(gsym0))
+      else gsym0
     }
 
     private val isObject = gsym.isModule && !gsym.hasFlag(gf.PACKAGE) && gsym.name != nme.PACKAGE
@@ -20,65 +21,64 @@ trait SymbolInformationOps { self: DatabaseOps =>
     private def symbol: String = gsym.toSemantic.syntax
 
     private def kind: s.SymbolInformation.Kind = {
-      if (gsym.isPrimaryConstructor) k.PRIMARY_CONSTRUCTOR
-      else if (gsym.isConstructor) k.SECONDARY_CONSTRUCTOR
-      else if (gsym.hasFlag(gf.MACRO)) k.MACRO
-      else if (gsym.isMethod && !gsym.hasFlag(gf.ACCESSOR) && !gsym.hasFlag(gf.PARAMACCESSOR))
-        k.DEF
-      else if (gsym.isType && !gsym.isClass && !gsym.hasFlag(gf.PARAM)) k.TYPE
-      else if (gsym.isTerm && (gsym.hasFlag(gf.PARAM) || gsym.hasFlag(gf.PARAMACCESSOR)))
-        k.PARAMETER
-      else if (gsym.isType && gsym.hasFlag(gf.PARAM)) k.TYPE_PARAMETER
-      else if (isObject) k.OBJECT
-      else if (gsym.hasFlag(gf.PACKAGE)) k.PACKAGE
-      else if (gsym.isModule && gsym.name == nme.PACKAGE) k.PACKAGE_OBJECT
-      else if (gsym.isClass && !gsym.hasFlag(gf.TRAIT)) k.CLASS
-      else if (gsym.isClass && gsym.hasFlag(gf.TRAIT)) k.TRAIT
-      else if (gsym.isTerm && (gsym.hasFlag(gf.MUTABLE) || nme.isSetterName(gsym.name))) k.VAR
-      else if (gsym.isTerm && !(gsym.hasFlag(gf.LOCAL) && gsym.hasFlag(gf.PARAMACCESSOR))) k.VAL
-      else k.UNKNOWN_KIND
-    }
-
-    private def accessQualifierProperties: Int = {
-      var flags = 0
-      val gpriv = gsym.privateWithin.orElse(gsym.owner)
-      if (gsym.hasFlag(gf.SYNTHETIC) && gsym.hasFlag(gf.ARTIFACT)) {
-        // NOTE: some sick artifact vals produced by mkPatDef can be private to method (whatever that means)
-        // so here we just ignore them
-      } else {
-        if (gsym.hasFlag(gf.PROTECTED)) flags |= p.PROTECTED.value
-        if (gsym.hasFlag(gf.PRIVATE) && !gsym.hasFlag(gf.PARAMACCESSOR)) flags |= p.PRIVATE.value
-        // TODO: `private[pkg] class C` doesn't have PRIVATE in its flags
-        // so we need to account for that!
-        if (gsym.hasAccessBoundary && gpriv != g.NoSymbol && !gsym.hasFlag(gf.PROTECTED))
-          flags |= p.PRIVATE.value
+      gsym match {
+        case gsym: MethodSymbol =>
+          if (gsym.isConstructor) {
+            if (gsym.isPrimaryConstructor) k.PRIMARY_CONSTRUCTOR
+            else k.SECONDARY_CONSTRUCTOR
+          } else {
+            if (gsym.isSetter) k.SETTER
+            else if (gsym.isGetter && gsym.isLazy && !gsym.isClass) k.VAL
+            else if (gsym.isGetter) k.GETTER
+            else if (gsym.isMacro) k.MACRO
+            else k.DEF
+          }
+        case gsym: ModuleSymbol =>
+          if (gsym.hasPackageFlag) k.PACKAGE
+          else if (gsym.isPackageObject) k.PACKAGE_OBJECT
+          else k.OBJECT
+        case gsym: TermSymbol =>
+          if (gsym.isSelfParameter) k.SELF_PARAMETER
+          else if (gsym.isParameter) k.PARAMETER
+          else if (gsym.isMutable) k.VAR
+          else k.VAL
+        case gsym: ClassSymbol =>
+          if (gsym.isTrait) k.TRAIT
+          else k.CLASS
+        case gsym: TypeSymbol =>
+          if (gsym.isParameter) k.TYPE_PARAMETER
+          else k.TYPE
+        case NoSymbol =>
+          k.UNKNOWN_KIND
+        case _ =>
+          sys.error(s"unsupported symbol $gsym")
       }
-      flags
-    }
-
-    private def otherProperties: Int = {
-      var flags = 0
-      val isDeclaredDeferred = gsym.hasFlag(gf.DEFERRED) && !gsym.hasFlag(gf.PARAM)
-      val isDeclaredAbstract = (gsym.hasFlag(gf.ABSTRACT) && !gsym.hasFlag(gf.TRAIT)) || gsym
-        .hasFlag(gf.ABSOVERRIDE)
-      if (isDeclaredDeferred || isDeclaredAbstract) flags |= p.ABSTRACT.value
-      if ((gsym.hasFlag(gf.FINAL) && !gsym.hasFlag(gf.PACKAGE)) || isObject) flags |= p.FINAL.value
-      if (gsym.hasFlag(gf.SEALED)) flags |= p.SEALED.value
-      if (gsym.hasFlag(gf.IMPLICIT)) flags |= p.IMPLICIT.value
-      if (gsym.hasFlag(gf.LAZY)) flags |= p.LAZY.value
-      if (gsym.hasFlag(gf.CASE)) flags |= p.CASE.value
-      if (gsym.isType && gsym.hasFlag(gf.CONTRAVARIANT)) flags |= p.CONTRAVARIANT.value
-      if (gsym.isType && gsym.hasFlag(gf.COVARIANT)) flags |= p.COVARIANT.value
-      // TODO: p.VALPARAM and p.VARPARAM
-      flags
     }
 
     private def properties: Int = {
-      accessQualifierProperties | otherProperties
+      var props = 0
+      def isAbstractClass = gsym.isClass && gsym.isAbstractClass && !gsym.isTrait
+      def isAbstractMethod = gsym.isMethod && gsym.isDeferred
+      def isAbstractType = gsym.isType && !gsym.isParameter && gsym.isDeferred
+      if (isAbstractClass || isAbstractMethod || isAbstractType) props |= p.ABSTRACT.value
+      if ((gsym.hasFlag(gf.FINAL) && !gsym.hasFlag(gf.PACKAGE)) || isObject) props |= p.FINAL.value
+      if (gsym.hasFlag(gf.SEALED)) props |= p.SEALED.value
+      if (gsym.hasFlag(gf.IMPLICIT)) props |= p.IMPLICIT.value
+      if (gsym.hasFlag(gf.LAZY)) props |= p.LAZY.value
+      if (gsym.hasFlag(gf.CASE)) props |= p.CASE.value
+      if (gsym.isType && gsym.hasFlag(gf.CONTRAVARIANT)) props |= p.CONTRAVARIANT.value
+      if (gsym.isType && gsym.hasFlag(gf.COVARIANT)) props |= p.COVARIANT.value
+      if (gsym.isParameter && gsym.owner.isPrimaryConstructor) {
+        val ggetter = gsym.getter(gsym.owner.owner)
+        if (ggetter != g.NoSymbol && !ggetter.isStable) props |= p.VARPARAM.value
+        else if (ggetter != g.NoSymbol) props |= p.VALPARAM.value
+        else ()
+      }
+      props
     }
 
     private def name: String = {
-      gsym.decodedName.toString
+      gsym.name.toSemantic
     }
 
     private def range: Option[s.Location] = None
@@ -97,6 +97,34 @@ trait SymbolInformationOps { self: DatabaseOps =>
 
     private def tpe: Option[s.Type] = None
 
+    private def anns: List[s.Annotation] = Nil
+
+    // TODO: I'm not completely happy with the implementation of this method.
+    // See https://github.com/scalameta/scalameta/issues/1325 for details.
+    private def acc: Option[s.Accessibility] = {
+      if (gsym.hasFlag(gf.SYNTHETIC) && gsym.hasFlag(gf.ARTIFACT)) {
+        // NOTE: some sick artifact vals produced by mkPatDef can be
+        // private to method (whatever that means), so here we just ignore them.
+        Some(s.Accessibility(a.PUBLIC))
+      } else {
+        if (gsym.privateWithin == NoSymbol) {
+          if (gsym.isPrivateLocal) Some(s.Accessibility(a.PRIVATE_THIS))
+          else if (gsym.isPrivate) Some(s.Accessibility(a.PRIVATE))
+          else if (gsym.isProtectedLocal) Some(s.Accessibility(a.PROTECTED_THIS))
+          else if (gsym.isProtected) Some(s.Accessibility(a.PROTECTED))
+          else Some(s.Accessibility(a.PUBLIC))
+        } else {
+          val ssym = gsym.privateWithin.toSemantic.syntax
+          if (gsym.isProtected) Some(s.Accessibility(a.PROTECTED_WITHIN, ssym))
+          else Some(s.Accessibility(a.PRIVATE_WITHIN, ssym))
+        }
+      }
+    }
+
+    private def owner: String = {
+      gsym.owner.toSemantic.syntax
+    }
+
     def toSymbolInformation: s.SymbolInformation = {
       s.SymbolInformation(
         symbol,
@@ -108,7 +136,10 @@ trait SymbolInformationOps { self: DatabaseOps =>
         signature,
         members,
         overrides,
-        tpe)
+        tpe,
+        anns,
+        acc,
+        owner)
     }
   }
 }
--- sbthost/nsc/src/main/scala/scala/meta/internal/sbthost/SymbolOps.scala
+++ sbthost/nsc/src/main/scala/scala/meta/internal/sbthost/SymbolOps.scala
@@ -4,91 +4,90 @@ import java.util._
 import org.{langmeta => m}
 import scala.reflect.internal.util.NoPosition
 
-trait InputOps { self: DatabaseOps =>
-  implicit class XtensionGPosition(pos: g.Position) {
-    def toInput: m.Input = {
-      if ((pos.source.file ne null) &&
-          (pos.source.file.file ne null)) {
-        val relativePath = config.sourceroot.relativize(pos.source.file.file.toPath)
-        m.Input.File(relativePath)
-      } else {
-        m.Input.None
-      }
-    }
-  }
-}
-
 trait SymbolOps { self: DatabaseOps =>
 
   private var nextId = 0
+  lazy val symbolCache = new HashMap[g.Symbol, m.Symbol]
   implicit class XtensionGSymbolMSymbol(sym: g.Symbol) {
     def toSemantic: m.Symbol = {
-      if (sym == null || sym == g.NoSymbol) return m.Symbol.None
-      // + semanticdb-scalac deviation
-      if (sym.isRoot) return m.Symbol.Global(m.Symbol.None, m.Signature.Term("_root_"))
-      // if (sym.isRootPackage) return m.Symbol.Global(m.Symbol.None, m.Signature.Term("_root_"))
-      // + semanticdb-scalac deviation
-      if (sym.isOverloaded) return m.Symbol.Multi(sym.alternatives.map(_.toSemantic))
-      if (sym.isModuleClass) return sym.asClass.module.toSemantic
-      if (sym.isEmptyPackage) return m.Symbol.Global(m.Symbol.None, m.Signature.Term("_empty_"))
+      def uncached(sym: g.Symbol): m.Symbol = {
+        if (sym == null || sym == g.NoSymbol) return m.Symbol.None
+        if (sym.isOverloaded) return m.Symbol.Multi(sym.alternatives.map(_.toSemantic))
+        // NOTE: In Scala 2.10, RootClass.module is NoSymbol.
+        // This is weird, but what can we do.
+        if (sym.isRoot) return g.definitions.RootPackage.toSemantic
+        if (sym.isModuleClass) return sym.asClass.module.toSemantic
+        if (sym.isTypeSkolem) return sym.deSkolemize.toSemantic
+        if (sym.isRootPackage) return m.Symbol.Global(m.Symbol.None, m.Signature.Term("_root_"))
+        if (sym.isEmptyPackage) return m.Symbol.Global(m.Symbol.None, m.Signature.Term("_empty_"))
 
-      def isLocal(sym: g.Symbol): Boolean = {
-        def definitelyGlobal = sym.hasPackageFlag
-        def definitelyLocal =
-          sym.name.decoded.startsWith(g.nme.LOCALDUMMY_PREFIX) ||
-            sym.name.decodedName == g.tpnme.REFINE_CLASS_NAME ||
-            (sym.owner.isMethod && !sym.isParameter) ||
-            ((sym.owner.isAliasType || sym.owner.isAbstractType) && !sym.isParameter)
-        !definitelyGlobal && (definitelyLocal || isLocal(sym.owner))
-      }
-      // + deviation from scalameta
-      if (isLocal(sym)) {
-        if (sym.pos == NoPosition) return m.Symbol.None
-        else {
-          val id = nextId
-          nextId += 1
-          m.Symbol.Local("local" + id.toString)
-        }
-      }
-      // - deviation from scalameta
-
-      val owner = sym.owner.toSemantic
-      val signature = {
-        def name(sym: g.Symbol) = sym.name.decoded.stripSuffix(g.nme.LOCAL_SUFFIX_STRING)
-        def jvmSignature(sym: g.MethodSymbol): String = {
-          // NOTE: unfortunately, this simple-looking facility generates side effects that corrupt the state of the compiler
-          // in particular, mixin composition stops working correctly, at least for `object Main extends App`
-          // val g = c.universe.asInstanceOf[scala.tools.nsc.Global]
-          // exitingDelambdafy(new genASM.JPlainBuilder(null, false).descriptor(sym))
-          def encode(tpe: g.Type): String = {
-            val g.TypeRef(_, sym, args) = tpe
-            require(args.isEmpty || sym == g.definitions.ArrayClass)
-            if (sym == g.definitions.UnitClass) "V"
-            else if (sym == g.definitions.BooleanClass) "Z"
-            else if (sym == g.definitions.CharClass) "C"
-            else if (sym == g.definitions.ByteClass) "B"
-            else if (sym == g.definitions.ShortClass) "S"
-            else if (sym == g.definitions.IntClass) "I"
-            else if (sym == g.definitions.FloatClass) "F"
-            else if (sym == g.definitions.LongClass) "J"
-            else if (sym == g.definitions.DoubleClass) "D"
-            else if (sym == g.definitions.ArrayClass) "[" + encode(args.head)
-            else "L" + sym.fullName.replace(".", "/") + ";"
+        if (sym.isSemanticdbLocal) {
+          return {
+            // + semanticdb-scalac deviation
+            if (sym.pos == g.NoPosition) m.Symbol.None
+            else {
+              val id = nextId
+              nextId += 1
+              m.Symbol.Local("local" + id.toString)
+            }
+            // - semanticdb-scalac deviation
           }
-          val g.MethodType(params, ret) = sym.info.erasure
-          val jvmRet = if (!sym.isConstructor) ret else g.definitions.UnitClass.toType
-          "(" + params.map(param => encode(param.info)).mkString("") + ")" + encode(jvmRet)
         }
 
-        if (sym.isMethod && !sym.asMethod.isGetter)
-          m.Signature.Method(name(sym), jvmSignature(sym.asMethod))
-        else if (sym.isTypeParameter) m.Signature.TypeParameter(name(sym))
-        else if (sym.isValueParameter || sym.isParamAccessor) m.Signature.TermParameter(name(sym))
-        else if (sym.owner.thisSym == sym) m.Signature.Self(name(sym))
-        else if (sym.isType) m.Signature.Type(name(sym))
-        else m.Signature.Term(name(sym))
+        val owner = sym.owner.toSemantic
+        val signature = {
+          if (sym.isMethod) {
+            m.Signature.Method(sym.name.toSemantic, sym.disambiguator)
+          } else if (sym.isTypeParameter) {
+            m.Signature.TypeParameter(sym.name.toSemantic)
+          } else if (sym.isValueParameter) {
+            m.Signature.TermParameter(sym.name.toSemantic)
+          } else if (sym.isType) {
+            m.Signature.Type(sym.name.toSemantic)
+          } else {
+            m.Signature.Term(sym.name.toSemantic)
+          }
+        }
+        m.Symbol.Global(owner, signature)
       }
-      m.Symbol.Global(owner, signature)
+      val msym = symbolCache.get(sym)
+      if (msym != null) {
+        msym
+      } else {
+        val msym = uncached(sym)
+        symbolCache.put(sym, msym)
+        msym
+      }
+    }
+  }
+
+  implicit class XtensionGSymbolMSpec(sym: g.Symbol) {
+    def isSemanticdbGlobal: Boolean = !isSemanticdbLocal
+    def isSemanticdbLocal: Boolean = {
+      def definitelyGlobal = sym.hasPackageFlag
+      def definitelyLocal =
+        sym == g.NoSymbol ||
+          sym.name.decoded.startsWith(g.nme.LOCALDUMMY_PREFIX) ||
+          (sym.owner.isMethod && !sym.isParameter) ||
+          ((sym.owner.isAliasType || sym.owner.isAbstractType) && !sym.isParameter) ||
+          sym.isSelfParameter
+      !definitelyGlobal && (definitelyLocal || sym.owner.isSemanticdbLocal)
+    }
+    def isSemanticdbMulti: Boolean = sym.isOverloaded
+    def descriptor: String = {
+      sym.info.descriptor
+    }
+    def disambiguator: String = {
+      val siblings = sym.owner.info.decls.sorted.filter(_.name == sym.name)
+      val synonyms = siblings.filter(_.descriptor == sym.descriptor)
+      val suffix = {
+        if (synonyms.length == 1) ""
+        else "+" + (synonyms.indexOf(sym) + 1)
+      }
+      "(" + descriptor + suffix + ")"
+    }
+    def isSelfParameter: Boolean = {
+      sym != g.NoSymbol && sym.owner.thisSym == sym
     }
   }
 }
